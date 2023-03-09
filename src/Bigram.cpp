b6363c0139521dbd4a200508b5119dd070809186
isaac <isaac@Isaac-Mint>
2023-03-09 01:30:41 +0100

    Created Language0 project

---
+++ src/Bigram.cpp
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ */
+
+/* 
+ * File:   Bigram.cpp
+ * Author: isaac
+ * 
+ * Created on 9 de marzo de 2023, 1:14
+ */
+
+#include "Bigram.h"
+
+Bigram::Bigram() {
+}
+
+Bigram::Bigram(const Bigram& orig) {
+}
+
+Bigram::~Bigram() {
+}
+
