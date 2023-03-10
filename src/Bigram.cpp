9a1a30912f248222c6e56d31fe0ab2927a101b7b
isaac <isaac@Isaac-Mint>
2023-03-10 11:32:58 +0100

    Implemented toUpper() function correctly

--- src/Bigram.cpp
+++ src/Bigram.cpp
@@ -9,18 +9,19 @@
  * 
  * Created on 9 de marzo de 2023, 1:14
  */
-
-#include "Bigram.h"
-
-//Â¿Hay que quitar el valor por defecto cuando declaras la funciÃ³n?
+#include <string>
+#include "Bigram.h" 
+//¿Hay que quitar el valor por defecto cuando declaras la función?
 Bigram::Bigram(const std::string& text) {
-    if(text.size()==2){
+    if(text.size()==2/* && isValidCharacter(text[0], validCharacters) && isValidCharacter(text[1], validCharacters)*/){
         _text = text;
-    } 
+    } else{
+        _text = "__";
+    }
 }
 
 Bigram::Bigram(char first, char second) {
-    _text = first+second;
+    _text = {first, second};
 }
 
 std::string Bigram::getText() const{
@@ -41,8 +42,8 @@ const char& Bigram::at(int index) const{
     }
 }
 char& Bigram::at(int index){
-    if (index<0 || index > 2){
-        throw std::out_of_range(std::string("const char& Bigram::at(int index) const: ")
+    if (index<0 || index > 1){
+        throw std::out_of_range(std::string("char& Bigram::at(int index): ")
                 + "invalid position " + std::to_string(index));
     }
     else{
@@ -51,9 +52,18 @@ char& Bigram::at(int index){
 }
 
 bool isValidCharacter(char character, const std::string& validCharacters){
-    return false;
+        return false;
 }
 
 void toUpper(Bigram &bigram){
+    int diff = 'a' - 'A';
+    int diff2 = 'á' - 'Á';
+    for (int i = 0; i <= 1; i++){
+        if (bigram.at(i) >= 'a' && bigram.at(i)<= 'z'){
+            bigram.at(i) = bigram.at(i) - diff;
+        } else if (bigram.at(i) >= 'á' && bigram.at(i)<= 'ü'){
+            bigram.at(i) = bigram.at(i) - diff2;
+    } 
+    }
     
 }
